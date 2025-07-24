3f446c936c5811d393be7f8f875ee99df4e7461f
algoo1 <algobotdata@gmail.com>
2025-07-24 20:58:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI
+from pydantic import BaseModel
 from transformers import CLIPProcessor, CLIPModel
 from PIL import Image
 import torch
+import requests
 import io
 
 app = FastAPI()
@@ -9,10 +11,13 @@ app = FastAPI()
 model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
 processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
 
-@app.post("/embed-image")
-async def embed_image(file: UploadFile = File(...)):
-    image_bytes = await file.read()
-    image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
+class Input(BaseModel):
+    image_url: str
+
+@app.post("/")
+async def embed_image(input: Input):
+    image_data = requests.get(input.image_url).content
+    image = Image.open(io.BytesIO(image_data)).convert("RGB")
     inputs = processor(images=image, return_tensors="pt")
     with torch.no_grad():
         features = model.get_image_features(**inputs)
